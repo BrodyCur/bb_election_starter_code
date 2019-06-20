74cabe0ea667c227fbb9d318c5dc58ea8bb6dfec
Brody Currie <brodycurrie@gmail.com>
2019-06-20 13:49:00 -0400

    Completed part 1

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+  
+  axios.get("https://bb-election-api.herokuapp.com/")
+  .then((response) => {
+    const candidates = response.data.candidates;
+    const candidatesList = document.querySelector('#candidates-list');
+
+    candidates.forEach(candidate => {
+      const dataElem = document.createElement('li');
+      dataElem.innerText = `Name: ${candidate.name}\nNumber of Votes: ${candidate.votes}\n\n`;
+      candidatesList.append(dataElem);
+    });
+
+    // console.log(response.data.candidates.[0].name)
+  })
+
 
 });
